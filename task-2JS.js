26fc97a3816ac159e7108eaf35f3180089aca2f8
Fauzi Amallul <fauziamallul@Fauzis-MacBook-Pro.local>
2022-07-25 23:53:17 +0700

    an update

--- task-2JS.js
+++ task-2JS.js
@@ -1,1 +1,9 @@
-hello
+// VARIABLES
+const batu_player = document.getElementById('batu-player');
+const kertas_player = document.getElementById('kertas-player');
+const gunting_player = document.getElementById('kertas-player');
+
+const batu_bot = document.getElementById('batu-bot');
+const kertas_bot = document.getElementById('kertas-bot');
+const gunting_bot = document.getElementById('gunting-bot');
+
